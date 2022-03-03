944c43ae47ff365cc75c78ad45336236fb0915a1
Sanjay Bhangar <sanjay@developmentseed.org>
2022-03-03 18:44:28 +0530

    remove references to acr

--- deployment/terraform/resources/output.tf
+++ deployment/terraform/resources/output.tf
@@ -14,10 +14,6 @@ output "resource_group" {
   value = azurerm_resource_group.osmseed.name
 }
 
-output "image_registry" {
-  value = data.azurerm_container_registry.osmseed.name
-}
-
 output "storage_connection_string" {
   value = azurerm_storage_account.osmseed.primary_connection_string
 }
