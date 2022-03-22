33ba73b404b56f506ce3103f863bb34e738d0f29
Sajjad Anwar <sajjadkm@gmail.com>
2022-03-22 12:38:04 +0000

    create replication storage container and provide envvars

--- deployment/terraform/resources/helm-osm-seed.tf
+++ deployment/terraform/resources/helm-osm-seed.tf
@@ -37,7 +37,6 @@ resource "helm_release" "osmseed" {
     name = "web.env.MAILER_FROM"
     value = var.mailerFrom
   }
-
   set {
     name = "web.env.MAILER_PORT"
     value = var.mailerPort
@@ -58,4 +57,20 @@ resource "helm_release" "osmseed" {
     value = var.osmseed_db_disk_size
   }
 
+  set {
+    name  = "AZURE_STORAGE_CONNECTION_STRING"
+    value = azurerm_storage_account.osmseed.primary_connection_string
+  }
+  set {
+    name  = "AZURE_STORAGE_ACCESS_KEY"
+    value = azurerm_storage_account.osmseed.primary_access_key
+  }
+  set {
+    name  = "AZURE_CONTAINER_NAME"
+    value = "replication"
+  }
+  set {
+    name = "AZURE_STORAGE_ACCOUNT"
+    value = "${local.storage}"
+  }
 }
