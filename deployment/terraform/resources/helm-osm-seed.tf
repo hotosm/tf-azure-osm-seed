19a0a5233bbd5fb01eba2ad64b924722a318bb79
Sanjay Bhangar <sanjay@developmentseed.org>
2022-03-30 17:44:23 -0700

    bump osm-seed version

--- deployment/terraform/resources/helm-osm-seed.tf
+++ deployment/terraform/resources/helm-osm-seed.tf
@@ -2,7 +2,7 @@ resource "helm_release" "osmseed" {
   name  = "osmseed-helm"
   repository = "https://devseed.com/osm-seed-chart"
   chart = "osm-seed"
-  version = "0.1.0-n650.h2d59159"
+  version = "0.1.0-n651.h653b5cd"
   wait = false
   depends_on = [
 
