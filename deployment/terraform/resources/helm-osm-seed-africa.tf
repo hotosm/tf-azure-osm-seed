23cd185e385037d4f48dbb36042864cd92b2229e
Dakota Benjamin <dakota.benjamin@hotosm.org>
2022-07-18 12:29:54 -0400

    Add africa instance

---
+++ deployment/terraform/resources/helm-osm-seed-africa.tf
@@ -0,0 +1,60 @@
+resource "helm_release" "osmseed" {
+  name  = "osmseed-helm"
+  repository = "https://devseed.com/osm-seed-chart"
+  chart = "osm-seed"
+  version = "0.1.0-n651.h653b5cd"
+  wait = false
+  depends_on = [
+
+  ]
+
+  values = [
+    "${file("${path.module}/values-africa.yaml")}"
+  ]
+
+  set {
+    name = "web.env.MAILER_ADDRESS"
+    value = var.mailerAddress
+  }
+
+  set {
+    name = "web.env.MAILER_DOMAIN"
+    value = var.mailerDomain
+  }
+
+  set {
+    name = "web.env.MAILER_USERNAME"
+    value = var.mailerUsername
+  }
+
+  set_sensitive {
+    name = "web.env.MAILER_PASSWORD"
+    value = var.mailerPassword
+  }
+
+  set {
+    name = "web.env.MAILER_FROM"
+    value = var.mailerFrom
+  }
+
+  set {
+    name = "web.env.MAILER_PORT"
+    value = var.mailerPort
+  }
+
+  set {
+    name = "db.persistenceDisk.AZURE_diskName"
+    value = azurerm_managed_disk.osmseed_db.name
+  }
+
+  set {
+    name = "db.persistenceDisk.AZURE_diskURI"
+    value = azurerm_managed_disk.osmseed_db.id
+  }
+
+  set {
+    name = "db.persistenceDisk.AZURE_diskSize"
+    value = var.osmseed_db_disk_size
+  }
+
+}
