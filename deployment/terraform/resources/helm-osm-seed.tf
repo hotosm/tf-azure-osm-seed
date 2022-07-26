15857a969470ab94e32d3b1dbb98184a31daec81
Dakota Benjamin <dakota.benjamin@hotosm.org>
2022-07-26 08:18:19 -0400

    Update osm-seed version

--- deployment/terraform/resources/helm-osm-seed.tf
+++ deployment/terraform/resources/helm-osm-seed.tf
@@ -2,7 +2,7 @@ resource "helm_release" "osmseed" {
   name  = "osmseed-helm"
   repository = "https://devseed.com/osm-seed-chart"
   chart = "osm-seed"
-  version = "0.1.0-n651.h653b5cd"
+  version = "0.1.0-n686.hf40b9be"
   wait = false
   depends_on = [
 
